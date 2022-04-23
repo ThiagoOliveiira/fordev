56bd7137d92ee4604fc466f091c303bad17af7a4
Thiago Oliveira <thiago.dev18@gmail.com>
2022-04-23 14:37:06 -0300

    move page initialization to helper method

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -4,21 +4,39 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../../lib/ui/page/pages.dart';
 
 void main() {
-  testWidgets('Should load with correct initial state', (WidgetTester tester) async {
-    final loginPage = MaterialApp(home:  LoginPage());
+  Future<void> loadPage(WidgetTester tester) async {
+    final loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
 
-    final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
-    expect(emailTextChildren, findsOneWidget, reason: 'when a TextFormField one text child, means it has no errors, since one of the childs is always the label text',);
-   
-    final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
-    expect(passwordTextChildren, findsOneWidget, reason: 'when a TextFormField one text child, means it has no errors, since one of the childs is always the label text',);
-  
+  testWidgets('Should load with correct initial state',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    final emailTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(
+      emailTextChildren,
+      findsOneWidget,
+      reason:
+          'when a TextFormField one text child, means it has no errors, since one of the childs is always the label text',
+    );
+
+    final passwordTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    expect(
+      passwordTextChildren,
+      findsOneWidget,
+      reason:
+          'when a TextFormField one text child, means it has no errors, since one of the childs is always the label text',
+    );
+
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(button.onPressed, null);
-  
   });
 
-
-
+  testWidgets('Should call validade with correct values',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+  });
 }
