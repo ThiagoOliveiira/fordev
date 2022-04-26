d37c9f82435c9248a65f528aa35fae31daacf80c
Thiago Oliveira <thiago.dev18@gmail.com>
2022-04-25 23:29:28 -0300

    ensure LoginPage enables button if form is valid

--- lib/ui/page/login/login_presenter.dart
+++ lib/ui/page/login/login_presenter.dart
@@ -1,6 +1,7 @@
 abstract class LoginPresenter {
   Stream get emailErrorStream;
   Stream get passwordErrorController;
+  Stream get isFormValidController;
 
   void validateEmail(String email);
   void validatePassword(String password);
