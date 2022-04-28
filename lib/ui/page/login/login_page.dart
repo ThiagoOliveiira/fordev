0b19e6e625e27217a9a9d83c53c48dc2b7168f6d
Thiago Silva <thiago.dev18@gmail.com>
2022-04-28 12:09:59 -0300

    ajuste loginPage

---
+++ lib/ui/page/login/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'login_presenter.dart';
+
+import '../../components/components.dart';
+
+class LoginPage extends StatelessWidget {
+  final LoginPresenter presenter;
+
+  LoginPage(this.presenter);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          LoginHeader(),
+          Headline1(text: 'Login'),
+          Padding(
+            padding: EdgeInsets.all(32),
+            child: Form(
+              child: Column(
+                children: [
+                  StreamBuilder<String>(
+                    stream: presenter.emailErrorStream,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          icon: Icon(Icons.email,
+                              color: Theme.of(context).primaryColorLight),
+                          errorText: snapshot.data?.isEmpty == true
+                              ? null
+                              : snapshot.data,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: presenter.validateEmail,
+                      );
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, bottom: 32),
+                    child: StreamBuilder<String>(
+                      stream: presenter.passwordErrorController,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Senha",
+                            icon: Icon(
+                              Icons.lock,
+                              color: Theme.of(context).primaryColorLight,
+                            ),
+                            errorText: snapshot.data?.isEmpty == true
+                                ? null
+                                : snapshot.data,
+                          ),
+                          obscureText: true,
+                          onChanged: presenter.validatePassword,
+                        );
+                      },
+                    ),
+                  ),
+                  StreamBuilder<bool>(
+                      stream: presenter.isFormValidController,
+                      builder: (context, snapshot) {
+                        return RaisedButton(
+                          onPressed: snapshot.data == true ? () {} : null,
+                          child: Text("Entrar".toUpperCase()),
+                        );
+                      }),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.person),
+                    label: Text("Criar Conta"),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/ui/page/login/login_presenter.dart
@@ -0,0 +1,8 @@
+abstract class LoginPresenter {
+  Stream get emailErrorStream;
+  Stream get passwordErrorController;
+  Stream get isFormValidController;
+
+  void validateEmail(String email);
+  void validatePassword(String password);
+}
